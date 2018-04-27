6c4ffc2ebbcacad510906a2c00a364b7b15e806e
hnxyhcwdl1003@163.com <hnxyhcwdl1003@163.com>
2018-04-27 16:44:12 +0800

    xz

--- src/main/java/com/p6spy/spring/boot/P6SpyAutoConfiguration.java
+++ src/main/java/com/p6spy/spring/boot/P6SpyAutoConfiguration.java
@@ -12,7 +12,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import com.p6spy.engine.spy.DefaultJdbcEventListenerFactory;
+import com.p6spy.engine.spy.JdbcEventListenerFactory;
+import com.p6spy.engine.spy.P6ConnectionPoolDataSource;
 import com.p6spy.engine.spy.P6DataSource;
+import com.p6spy.engine.spy.P6ModuleManager;
 import com.p6spy.engine.spy.option.P6OptionChangedListener;
 import com.p6spy.spring.boot.ext.P6spyDataSource;
 
@@ -24,19 +28,30 @@ import com.p6spy.spring.boot.ext.P6spyDataSource;
 @EnableConfigurationProperties({ P6SpyProperties.class, DataSourceProperties.class})
 public class P6SpyAutoConfiguration {
 	
-	
 	@Bean
 	@ConditionalOnMissingBean
 	protected P6OptionChangedListener listener() {
 		return null;
 	}
 	
+	@Bean
+	@ConditionalOnMissingBean
+	protected JdbcEventListenerFactory jdbcEventListenerFactory() {
+		return new DefaultJdbcEventListenerFactory();
+	}
+	
 	@Primary
-	public P6DataSource druidDataSource(@P6spyDataSource ObjectProvider<DataSource> p6spyDataSource) {
+	public P6DataSource druidDataSource(@P6spyDataSource ObjectProvider<DataSource> p6spyDataSource,
+			JdbcEventListenerFactory jdbcEventListenerFactory) {
+		
+		P6ConnectionPoolDataSource p = new P6ConnectionPoolDataSource();
+		p.setJdbcEventListenerFactory(jdbcEventListenerFactory);
 		
 		//P6ModuleManager.getInstance().registerOptionChangedListener();
-				
-		return new P6DataSource(p6spyDataSource.getObject());
+		P6DataSource p6DataSource = new P6DataSource(p6spyDataSource.getObject());
+		p6DataSource.setJdbcEventListenerFactory(jdbcEventListenerFactory);
+		
+		return p6DataSource;
 	}
 
 }
