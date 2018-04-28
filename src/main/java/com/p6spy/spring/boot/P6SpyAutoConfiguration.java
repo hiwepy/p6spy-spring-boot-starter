096d3ffcc3bed10ca4984174aface83beafb921d
hnxyhcwdl1003@163.com <hnxyhcwdl1003@163.com>
2018-04-28 15:29:04 +0800

    逻辑调整

--- src/main/java/com/p6spy/spring/boot/P6SpyAutoConfiguration.java
+++ src/main/java/com/p6spy/spring/boot/P6SpyAutoConfiguration.java
@@ -5,7 +5,6 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -14,9 +13,7 @@ import org.springframework.context.annotation.Primary;
 
 import com.p6spy.engine.spy.DefaultJdbcEventListenerFactory;
 import com.p6spy.engine.spy.JdbcEventListenerFactory;
-import com.p6spy.engine.spy.P6ConnectionPoolDataSource;
 import com.p6spy.engine.spy.P6DataSource;
-import com.p6spy.engine.spy.P6ModuleManager;
 import com.p6spy.engine.spy.option.P6OptionChangedListener;
 import com.p6spy.spring.boot.ext.P6spyDataSource;
 
@@ -24,7 +21,6 @@ import com.p6spy.spring.boot.ext.P6spyDataSource;
  */
 @Configuration
 @ConditionalOnClass(com.p6spy.engine.spy.P6DataSource.class)
-@ConditionalOnProperty(name = "spring.datasource.p6spy.enabled" , havingValue = "true", matchIfMissing = true)
 @EnableConfigurationProperties({ P6SpyProperties.class, DataSourceProperties.class})
 public class P6SpyAutoConfiguration {
 	
@@ -43,9 +39,6 @@ public class P6SpyAutoConfiguration {
 	@Primary
 	public P6DataSource druidDataSource(@P6spyDataSource ObjectProvider<DataSource> p6spyDataSource,
 			JdbcEventListenerFactory jdbcEventListenerFactory) {
-		
-		P6ConnectionPoolDataSource p = new P6ConnectionPoolDataSource();
-		p.setJdbcEventListenerFactory(jdbcEventListenerFactory);
 		
 		//P6ModuleManager.getInstance().registerOptionChangedListener();
 		P6DataSource p6DataSource = new P6DataSource(p6spyDataSource.getObject());
