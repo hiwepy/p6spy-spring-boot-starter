fc777f58fdc15bdd040828d14205b5801e48237b
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 15:55:33 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/com/p6spy/spring/boot/P6SpyAutoConfiguration.java
+++ src/main/java/com/p6spy/spring/boot/P6SpyAutoConfiguration.java
@@ -21,7 +21,7 @@ import com.p6spy.spring.boot.ext.P6spyDataSource;
 /**
  * Auto-configuration for P6Spy.
  *
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  * @since 1.0.0
  */
 @Configuration
